c62a729a012f4ccfcc71d3c2a1d96c7abe115300
Eliz_matsen <macenko1996@gmail.com>
2021-03-04 10:07:41 +0300

    To deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+ git push -f git@github.com:ElizMatsen/ElizMatsen.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ElizMatsen.github.io/ElizMatsen.github.io master:gh-pages
+#git push -f git@github.com:ElizMatsen.github.io/ElizMatsen.github.io master:gh-pages
 
 cd -
